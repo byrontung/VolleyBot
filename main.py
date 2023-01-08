1c76d4980e6b2b2743a8d984f4b2c9626377bafb
byrontung <btung1@ualberta.ca>
2023-01-07 21:50:27 -0700

    bot template created

---
+++ main.py
@@ -0,0 +1,21 @@
+# This example requires the 'message_content' intent.
+
+"""
+Taken from the docs:
+https://discordpy.readthedocs.io/en/latest/intro.html#installing
+"""
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('my token goes here')
